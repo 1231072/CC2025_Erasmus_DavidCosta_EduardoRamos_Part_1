723303d8f925f191d8c77aef8b3c1506cc033254
david <1231072@isep.ipp.pt>
2025-11-20 20:47:08 +0200

    Changes on application.properties and pom.xml

--- src/main/java/com/cloudcomputing/config/security/SecurityConfig.java
+++ src/main/java/com/cloudcomputing/config/security/SecurityConfig.java
@@ -74,6 +74,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:3000",
                 "http://127.0.0.1:3000",
+                "http://localhost:8080",
                 "https://erasmus-cc2025-ui.azurewebsites.net"
         ));
 

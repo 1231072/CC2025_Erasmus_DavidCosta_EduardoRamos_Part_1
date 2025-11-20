46f37e6cfa5a65e74fb12b39b7da117667fc9476
david <1231072@isep.ipp.pt>
2025-11-21 01:55:35 +0200

    Migrated the UI to a nodejs server

--- src/main/java/com/cloudcomputing/CloudComputingApplication.java
+++ src/main/java/com/cloudcomputing/CloudComputingApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class CloudComputingApplication {
 
     public static void main(String[] args) {
